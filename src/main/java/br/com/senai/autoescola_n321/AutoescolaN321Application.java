083be2715730f0957a77bda57279e21beef7abe3
izabelydev <izabelydev@github.com>
2025-10-22 19:07:11 -0300

    feat: configuracoes iniciais springboot

---
+++ src/main/java/br/com/senai/autoescola_n321/AutoescolaN321Application.java
@@ -0,0 +1,13 @@
+package br.com.senai.autoescola_n321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoescolaN321Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoescolaN321Application.class, args);
+	}
+
+}
