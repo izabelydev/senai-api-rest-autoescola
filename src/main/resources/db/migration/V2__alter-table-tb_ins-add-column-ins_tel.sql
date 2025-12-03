1dfed99e7f192df7e1ca5b56145570e35b12a300
izabelydev <izabelydev@github.com>
2025-12-03 19:01:12 -0300

    feat: remocao de massa de teste do banco de dados

--- src/main/resources/db/migration/V2__alter-table-tb_ins-add-column-ins_tel.sql
+++ src/main/resources/db/migration/V2__alter-table-tb_ins-add-column-ins_tel.sql
@@ -1,17 +1,1 @@
 ALTER TABLE tb_ins ADD COLUMN ins_tel VARCHAR(20) NOT NULL;
-
-UPDATE tb_ins SET ins_tel = '(11) 93456-7890' WHERE ins_eml = 'eduardosantos@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(11) 99876-5432' WHERE ins_eml = 'mariana.oliveira@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(19) 91234-5678' WHERE ins_eml = 'carlos.souza@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(41) 99871-2345' WHERE ins_eml = 'fernanda.lima@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(21) 91112-2233' WHERE ins_eml = 'lucas.almeida@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(47) 92223-3344' WHERE ins_eml = 'patricia.ribeiro@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(62) 93334-4455' WHERE ins_eml = 'rodrigo.martins@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(62) 94445-5566' WHERE ins_eml = 'juliana.costa@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(31) 95556-6677' WHERE ins_eml = 'andre.pereira@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(21) 96667-7788' WHERE ins_eml = 'renata.carvalho@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(21) 97778-8899' WHERE ins_eml = 'thiago.nunes@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(51) 98889-9900' WHERE ins_eml = 'camila.torres@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(81) 99900-0111' WHERE ins_eml = 'bruno.ferreira@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(31) 90001-1122' WHERE ins_eml = 'ana.mendes@autoescola.com.br';
-UPDATE tb_ins SET ins_tel = '(51) 91112-2233' WHERE ins_eml = 'gabriel.barbosa@autoescola.com.br';
